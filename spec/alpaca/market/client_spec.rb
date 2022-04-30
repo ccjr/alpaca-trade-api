fa7fdcf4c47f03158494fec27f2a553b4343b22f
Eduardo Carneiro <ecarneiro@decisiv.com>
2022-04-30 12:37:17 -0300

    Stub Api configuration for Alpaca::Market::Client

--- spec/alpaca/market/client_spec.rb
+++ spec/alpaca/market/client_spec.rb
@@ -2,6 +2,14 @@
 
 RSpec.describe Alpaca::Market::Client do
   describe '#crypto_bars' do
+    let(:api_configuration) do
+      instance_double(Alpaca::Trade::Api::Configuration, key_id: 'KEY_ID', key_secret: 'KEY_S3CRET')
+    end
+
+    before do
+      allow(Alpaca::Trade::Api).to receive(:configuration).and_return(api_configuration)
+    end
+
     it 'returns an array of Bar objects', :vcr do
       bars = subject.crypto_bars('SOLUSD', '1Min')
       expect(bars).to be_an(Array)
